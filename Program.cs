8100c3413a3f5859062626d7b6b0e90ea1757639
Peter Jun Park <park@hey.com>
2024-01-25 00:10:40 -0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿Person peter = new("Peter", 28);
+Console.WriteLine(peter.Name + ", " + peter.Age);
+Console.ReadKey();
+
+internal class Person
+{
+    public string Name { get; }
+    public int Age { get; }
+
+    public Person(string name, int age)
+    {
+        if (name == string.Empty)
+        {
+            throw new Exception("Name is required.");
+        }
+        Name = name;
+
+        if (age < 0)
+        {
+            throw new Exception("age must be a positive integer.");
+        }
+        Age = age;
+    }
+}
